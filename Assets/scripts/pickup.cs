085579a61901ba67d79a2d76e28c0b09e3387244
jackdelahunt <45426048+jackdelahunt@users.noreply.github.com>
2020-04-11 18:59:46 +0100

    initial commit

---
+++ Assets/scripts/pickup.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+    [SerializeField] GameObject hand;
+    [SerializeField] Material hoverMaterial;
+    GameObject heldObject;
+    RaycastHit hit;
+
+    private void FixedUpdate()
+    {
+        dropItem();
+
+        pickupItem();
+    }
+
+    void dropItem()
+    {
+        if(Input.GetKeyDown("e"))
+            if(heldObject != null)
+            {
+                Rigidbody heldObjectRigidbody = heldObject.GetComponent<Rigidbody>();
+                heldObjectRigidbody.useGravity = true;
+                heldObject = null;
+            }
+    }
+
+    void pickupItem()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if (heldObject != null)
+        {
+            heldObject.transform.position = hand.transform.position;
+            Rigidbody heldObjectRigidbody = heldObject.GetComponent<Rigidbody>();
+            heldObjectRigidbody.useGravity = false;
+        }
+        else
+        {
+
+            if (Physics.Raycast(transform.position, fwd, out hit, 10))
+            {
+                print(hit.collider.tag);
+                if (hit.collider.tag == "Item")
+                {
+
+
+                    if (Input.GetKeyDown("e"))
+                    {
+                        heldObject = hit.collider.gameObject;
+                    }
+                }
+            }
+        }
+            
+    }
+
+
+}
---
+++ Assets/scripts/toggleLight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggleLight : MonoBehaviour
+{
+    [SerializeField] Light spotLight;
+    [SerializeField] AudioSource buttonPress;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            buttonPress.Play();
+
+            if (spotLight.enabled)
+                spotLight.enabled = false;
+            else
+            {
+                spotLight.enabled = true;
+            }
+        }
+    }
+}
